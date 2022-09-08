a86d775a938aba0ab4a111d568ad15a7e8ea60b9
Liber022 <emmyarsiah1993@gmail.com>
2022-09-08 15:42:18 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+/**
+ * main-entry: function which is the entry point for running programs
+ *
+ * Description: This wil print a message to the screen
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle");
+	return (0);
+}
