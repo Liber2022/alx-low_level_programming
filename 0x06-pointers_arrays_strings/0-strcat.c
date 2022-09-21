4458cc2b0eec16f65da6c03354b9234edea01ee3
Liber022 <emmyarsiah1993@gmail.com>
2022-09-21 11:27:18 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - functiion that swap two integers 
+ * @a: first pointer int
+ * @b: second pointer int
+ * Return: nothing(void)
+ */
+
+   void swap_int(int *a, int *b)
+   {
+	   	int 1;
+
+		i = *a;
+		*a = *b;
+		*b = i;
+   } 
